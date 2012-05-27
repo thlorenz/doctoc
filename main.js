421f9199f2b13f4f300eeeec53f8537aa57fc2c6
Thorsten Lorenz <thlorenz@gmx.de>
2012-05-27 15:46:53 -0400

    getting all file content

--- main.js
+++ main.js
@@ -15,10 +15,29 @@ function isAbsolute(path) {
 
 var target = isAbsolute(dir) ? dir : path.join(__dirname, dir);
 
-function transformAndSave(files) {
+function transformAndSave(files, fcb) {
     console.log('Adding table of content to %s files.', files.length);
+
+    async.forEach(
+        files, 
+        function (f, cb) {
+            fs.readFile(f.path, 'utf8', function (err, content) {
+                if (err) { console.log(err); cb();  return; }
+                transform(f, content, cb);
+            });
+        },
+        fcb);
 }
 
-file.findMarkdownFiles(target, transformAndSave); 
+function transform (f, content, cb) {
+    console.log('transforming', f);
+    console.log(content);
+    cb();
+}
 
+
+
+file.findMarkdownFiles(target, function (files) {
+    transformAndSave(files, function () { console.log('Everything is OK'); }); 
+});
 
