9906e18a947839b544d6ebae895f2465e5c749c3
Thorsten Lorenz <thlorenz@gmx.de>
2013-03-31 21:56:10 -0400

    ugrading markdown-header and handling 'bitbucket.org' in transform module

--- test/lib/transform.js
+++ test/lib/transform.js
@@ -4,11 +4,10 @@
 var test = require('trap').test
   , transform = require('../../lib/transform')
 
-function check(md, anchors) {
+function check(md, anchors, mode) {
   test('transforming ' + md , function (t) {
-    var res = transform(md)
+    var res = transform(md, mode)
     t.ok(res.transformed, 'transforms it')     
-    console.log(require('util').inspect(res.data))
     t.equal(res.data, anchors + md, 'generates correct anchors')
   })
 }
@@ -72,4 +71,25 @@ check(
     , '- [Title should be included](#title-should-be-included)\n'
     , '- [Title should also be included](#title-should-also-be-included)\n\n'
     ].join('')
+)
+
+// bigbucket.org
+check(
+    [ '# My Module'
+    , 'Some text here'
+    , '## API'
+    , '### Method One'
+    , 'works like this'
+    , '### Method Two'
+    , '#### Main Usage'
+    , 'some main usage here'
+    ].join('\n')
+  , [ '**Table of Contents**  *generated with [DocToc](http://doctoc.herokuapp.com/)*\n\n'
+    , '- [My Module](#markdown-header-my-module)\n'
+    ,   '\t- [API](#markdown-header-api)\n'
+    ,     '\t\t- [Method One](#markdown-header-method-one)\n'
+    ,     '\t\t- [Method Two](#markdown-header-method-two)\n'
+    ,         '\t\t\t- [Main Usage](#markdown-header-main-usage)\n\n'
+    ].join('')
+  , 'bitbucket.org'
 )
