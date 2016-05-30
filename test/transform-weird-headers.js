f6f7a02dd31fabb9a6ec962184fae4a00b8aa1dd
Jacob Zimmerman <zimmerman.jake@gmail.com>
2016-05-29 23:11:15 -0700

    Upgrade markdown-to-ast
    Fixes #94.
    
    An upstream bug was affecting our usage of markdown-to-ast. It was fixed
    upstream, so we're upgrading our dependency to the latest version.
    
    We also add a minimal test readme that previously failed and which no
    longer fails after the change.

--- test/transform-weird-headers.js
+++ test/transform-weird-headers.js
@@ -20,3 +20,21 @@ test('\ngiven a file with edge-case header names', function (t) {
 
   t.end()
 })
+
+test('\nnameless table headers', function (t) {
+  var content = require('fs').readFileSync(__dirname + '/fixtures/readme-nameless-table-headers.md', 'utf8');
+  var headers = transform(content);
+
+  t.deepEqual(
+      headers.toc.split('\n')
+    , [ '**Table of Contents**  *generated with [DocToc](https://github.com/thlorenz/doctoc)*',
+        '',
+        '- [Heading One](#heading-one)',
+        '  - [Subheading 2](#subheading-2)',
+        '' ]
+    , 'generates a correct toc when readme has nameless table headers'
+  )
+
+  t.end()
+})
+
