c9ec297b328573b5884bdd6f639171f61d1463fa
Esref Durna <edurna@gmail.com>
2017-06-11 00:26:27 +0000

    eslint fixes: transform-stdout.js    1:13  error  Extra semicolon                                               semi    2:1   error  Expected exception block, space or tab after '/*' in comment  spaced-comment    5:5   error  Expected indentation of 2 spaces but found 4                  indent    6:5   error  Expected indentation of 2 spaces but found 4                  indent    6:20  error  Strings must use singlequote                                  quotes    6:41  error  Extra semicolon                                               semi    8:68  error  Block must not be padded by blank lines                       padded-blocks   10:5   error  Expected indentation of 2 spaces but found 4                  indent   12:45  error  Extra semicolon                                               semi   13:15  error  Extra semicolon                                               semi   23:62  error  Block must not be padded by blank lines                       padded-blocks   25:5   error  Expected indentation of 2 spaces but found 4                  indent   27:45  error  Extra semicolon                                               semi   28:15  error  Extra semicolon                                               semi
    ✖ 14 problems (14 errors, 0 warnings)

--- test/transform-stdout.js
+++ test/transform-stdout.js
@@ -1,36 +1,34 @@
-'use strict';
-/*jshint asi: true */
+'use strict'
+/* jshint asi: true */
 
 var test = require('tap').test,
-    fs = require('fs'),
-    exec = require("child_process").exec;
+  fs = require('fs'),
+  exec = require('child_process').exec
 
 test('\nshould print to stdout with --stdout option', function (t) {
-
-    exec('node doctoc.js test/fixtures/readme-with-custom-title.md --stdout', function (error, stdout, stderr) {
-      if (error) {
-        console.error('exec error: ', error);
-        return;
-      }
-      t.deepEqual(stdout
+  exec('node doctoc.js test/fixtures/readme-with-custom-title.md --stdout', function (error, stdout, stderr) {
+    if (error) {
+      console.error('exec error: ', error)
+      return
+    }
+    t.deepEqual(stdout
         , fs.readFileSync(__dirname + '/fixtures/stdout.md', 'utf8')
         , 'spits out the correct table of contents')
 
-      t.end()
-    })
+    t.end()
+  })
 })
 
 test('\nshould print to stdout with -s option', function (t) {
-
-    exec('node doctoc.js test/fixtures/readme-with-custom-title.md -s', function (error, stdout, stderr) {
-      if (error) {
-        console.error('exec error: ', error);
-        return;
-      }
-      t.deepEqual(stdout
+  exec('node doctoc.js test/fixtures/readme-with-custom-title.md -s', function (error, stdout, stderr) {
+    if (error) {
+      console.error('exec error: ', error)
+      return
+    }
+    t.deepEqual(stdout
         , fs.readFileSync(__dirname + '/fixtures/stdout.md', 'utf8')
         , 'spits out the correct table of contents')
 
-      t.end()
-    })
+    t.end()
+  })
 })
