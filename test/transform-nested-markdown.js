43d7ed7ac7770c1220eeb82b63d4ce911b76948d
Esref Durna <edurna@gmail.com>
2017-06-11 00:26:27 +0000

    eslint fixes: transform-nested-markdown.js    1:13   error  Extra semicolon                                               semi    2:1    error  Expected exception block, space or tab after '/*' in comment  spaced-comment    5:4    error  ',' should be placed last                                     comma-style    5:44   error  Extra semicolon                                               semi    8:107  error  Extra semicolon                                               semi    9:58   error  Extra semicolon                                               semi   14:9    error  Expected indentation of 6 spaces but found 8                  indent   15:9    error  Expected indentation of 6 spaces but found 8                  indent   16:9    error  Expected indentation of 6 spaces but found 8                  indent   17:9    error  Expected indentation of 6 spaces but found 8                  indent   18:9    error  Expected indentation of 6 spaces but found 8                  indent   19:9    error  Expected indentation of 6 spaces but found 8                  indent   20:9    error  Expected indentation of 6 spaces but found 8                  indent   20:11   error  Unexpected trailing comma                                     comma-dangle   21:7    error  Expected indentation of 4 spaces but found 6                  indent   25:3    error  Extra semicolon                                               semi   26:1    error  Too many blank lines at the end of file. Max of 0 allowed     no-multiple-empty-lines
    ✖ 17 problems (17 errors, 0 warnings)

--- test/transform-nested-markdown.js
+++ test/transform-nested-markdown.js
@@ -1,28 +1,25 @@
-'use strict';
-/*jshint asi: true */
+'use strict'
+/* jshint asi: true */
 
-var test = require('tap').test
-  , transform = require('../lib/transform');
+var test = require('tap').test,
+  transform = require('../lib/transform')
 
 test('\nhandle inline links and images', function (t) {
-  var content = require('fs').readFileSync(__dirname + '/fixtures/readme-with-nested-markdown.md', 'utf8');
-  var headers = transform(content, null, null, '', false);
+  var content = require('fs').readFileSync(__dirname + '/fixtures/readme-with-nested-markdown.md', 'utf8')
+  var headers = transform(content, null, null, '', false)
 
   t.deepEqual(
       headers.toc.split('\n')
     , [
-        '**Table of Contents**  *generated with [DocToc](https://github.com/thlorenz/doctoc)*',
-        '',
-        '- [CNN](#cnn)',
-        '- [Get Involved *](#get-involved-)',
-        '- [Translation Status *](#translation-status-)',
-        '- [Building *](#building-)',
-        '',
-      ]
+      '**Table of Contents**  *generated with [DocToc](https://github.com/thlorenz/doctoc)*',
+      '',
+      '- [CNN](#cnn)',
+      '- [Get Involved *](#get-involved-)',
+      '- [Translation Status *](#translation-status-)',
+      '- [Building *](#building-)',
+      ''
+    ]
     , 'generates correct toc for nested markdown (links and images)'
   )
   t.end()
-});
-
-
-
+})
