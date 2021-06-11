c06b5f324c05f131bc037813484dee12f5bb14d6
Frank <472730949@qq.com>
2021-06-11 14:53:48 +0800

    Add unit test for skip function

---
+++ test/fixtures/readme-with-skipTag.md
@@ -0,0 +1,9 @@
+# Hello, world!
+
+README to test doctoc with skipTag.
+
+<!-- DOCTOC SKIP -->
+
+## Installation
+## API
+## License
---
+++ test/transform-skipTag.js
@@ -0,0 +1,17 @@
+'use strict';
+/*jshint asi: true */
+
+var test = require('tap').test
+  , transform = require('../lib/transform');
+
+test('\nskip file transform', function (t) {
+  var content = require('fs').readFileSync(__dirname + '/fixtures/readme-with-skipTag.md', 'utf8');
+  var transformedContent = transform(content);
+
+  t.deepEqual(
+    transformedContent.toc
+    , undefined
+    , 'skip correct file'
+  )
+  t.end()
+});
