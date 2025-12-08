thlorenz/doctoc

commit b115924e62fd1cf470a8d0b0f37da22734679f84

Handle file skipping internally (#143)
The current solution for skipping files is to have a system utility such
as `ack` handle the searching for you.  This is not ideal because it
makes the documentation not as clear and ads a dependancies in some
cases.

Here we have the library handle skipping files, but simply not
transforming files that contain the tag

Co-authored-by: Andrew Smith <andrewsmith@alumni.stanford.edu>

lib/transform.js
@@ -9,6 +9,8 @@ var _             = require('underscore')
 var start = '<!-- START doctoc generated TOC please keep comment here to allow auto update -->\n' +
             '<!-- DON\'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->'
   , end   = '<!-- END doctoc generated TOC please keep comment here to allow auto update -->'
+  , skipTag = '<!-- DOCTOC SKIP -->';
+
 
 function matchesStart(line) {
   return (/<!-- START doctoc /).test(line);
@@ -107,6 +109,8 @@ function determineTitle(title, notitle, lines, info) {
 }
 
 exports = module.exports = function transform(content, mode, maxHeaderLevel, title, notitle, entryPrefix, processAll, updateOnly) {
+  if (content.indexOf(skipTag) !== -1) return { transformed: false };
+
   mode = mode || 'github.com';
   entryPrefix = entryPrefix || '-';
 

commit dfa656bd72478baafa9ba1ff8e00a3d706125dec

fix: Add in checks for line breaks around skip tag #291 (#292)
Closes #291

This improves the robustness of the skip tag so that it is ensured that only the intended files are being skipped.

This ensures that the project readme toc is kept upto date.

This issue was introduced with #143 and observed when working on #293

lib/transform.js
@@ -9,7 +9,7 @@ var _             = require('underscore')
 var start = '<!-- START doctoc generated TOC please keep comment here to allow auto update -->\n' +
             '<!-- DON\'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->'
   , end   = '<!-- END doctoc generated TOC please keep comment here to allow auto update -->'
-  , skipTag = '<!-- DOCTOC SKIP -->';
+  , skipTag = '<!-- DOCTOC SKIP -->\n';
 
 
 function matchesStart(line) {
@@ -109,7 +109,8 @@ function determineTitle(title, notitle, lines, info) {
 }
 
 exports = module.exports = function transform(content, mode, maxHeaderLevel, minHeaderLevel, title, notitle, entryPrefix, processAll, updateOnly) {
-  if (content.indexOf(skipTag) !== -1) return { transformed: false };
+  var index = content.indexOf(skipTag);
+  if (index === 0 || (index >= 0 && content[index-1] === '\n')) return { transformed: false };
 
   mode = mode || 'github.com';
   entryPrefix = entryPrefix || '-';
@@ -119,7 +120,7 @@ exports = module.exports = function transform(content, mode, maxHeaderLevel, min
   var maxHeaderLevelHtml = maxHeaderLevel || 4;
 
   var lines = content.split('\n')
-    , info = updateSection.parse(lines, matchesStart, matchesEnd)
+    , info = updateSection.parse(lines, matchesStart, matchesEnd);
 
   if (!info.hasStart && updateOnly) {
     return { transformed: false };

test/transform-skipTag.js
@@ -4,7 +4,7 @@
 var test = require('tap').test
   , transform = require('../lib/transform');
 
-test('\nskip file transform', function (t) {
+test('\nskip tag non first line transform', function (t) {
   var content = require('fs').readFileSync(__dirname + '/fixtures/readme-with-skipTag.md', 'utf8');
   var transformedContent = transform(content);
 
@@ -12,6 +12,30 @@ test('\nskip file transform', function (t) {
     transformedContent.toc
     , undefined
     , 'skip correct file'
+  )
+  t.end()
+});
+
+test('\nskip tag first line transform', function (t) {
+  var content = require('fs').readFileSync(__dirname + '/fixtures/readme-with-firstline-skipTag.md', 'utf8');
+  var transformedContent = transform(content);
+
+  t.deepEqual(
+    transformedContent.toc
+    , undefined
+    , 'skip correct file'
+  )
+  t.end()
+});
+
+test('\nskip tag invalid nested transform', function (t) {
+  var content = require('fs').readFileSync(__dirname + '/fixtures/readme-with-invalidskipTag.md', 'utf8');
+  var transformedContent = transform(content);
+
+  t.deepEqual(
+    transformedContent.transformed
+    , true
+    , 'don\'t skip file as skipTag is invalid'
   )
   t.end()
 });
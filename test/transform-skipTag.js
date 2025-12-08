dfa656bd72478baafa9ba1ff8e00a3d706125dec
James Thompson <thompson.tomo@outlook.com>
2025-12-08 11:14:17 +1100

    fix: Add in checks for line breaks around skip tag #291 (#292)
    Closes #291
    
    This improves the robustness of the skip tag so that it is ensured that only the intended files are being skipped.
    
    This ensures that the project readme toc is kept upto date.
    
    This issue was introduced with #143 and observed when working on #293

--- test/transform-skipTag.js
+++ test/transform-skipTag.js
@@ -4,7 +4,7 @@
 var test = require('tap').test
   , transform = require('../lib/transform');
 
-test('\nskip file transform', function (t) {
+test('\nskip tag non first line transform', function (t) {
   var content = require('fs').readFileSync(__dirname + '/fixtures/readme-with-skipTag.md', 'utf8');
   var transformedContent = transform(content);
 
@@ -12,6 +12,30 @@ test('\nskip file transform', function (t) {
     transformedContent.toc
     , undefined
     , 'skip correct file'
+  )
+  t.end()
+});
+
+test('\nskip tag first line transform', function (t) {
+  var content = require('fs').readFileSync(__dirname + '/fixtures/readme-with-firstline-skipTag.md', 'utf8');
+  var transformedContent = transform(content);
+
+  t.deepEqual(
+    transformedContent.toc
+    , undefined
+    , 'skip correct file'
+  )
+  t.end()
+});
+
+test('\nskip tag invalid nested transform', function (t) {
+  var content = require('fs').readFileSync(__dirname + '/fixtures/readme-with-invalidskipTag.md', 'utf8');
+  var transformedContent = transform(content);
+
+  t.deepEqual(
+    transformedContent.transformed
+    , true
+    , 'don\'t skip file as skipTag is invalid'
   )
   t.end()
 });
