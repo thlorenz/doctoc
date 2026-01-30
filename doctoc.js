02288b6868e8c8c02c5119ad2f4465944a91a2ba
James Thompson <thompson.tomo@outlook.com>
2026-01-31 00:40:11 +1100

    Cleanup argument parsing for minHeaderLevel

--- doctoc.js
+++ doctoc.js
@@ -94,7 +94,7 @@ var mode = modes['github'];
 
 var argv = minimist(process.argv.slice(2)
     , { boolean: [ 'h', 'help', 'T', 'notitle', 's', 'stdout', 'all' , 'u', 'update-only', 'd', 'dryrun'].concat(Object.keys(modes))
-    , string: [ 'title', 't', 'maxlevel', 'm', 'minlevel', 'n', 'entryprefix' ]
+    , string: [ 'title', 't', 'maxlevel', 'm', 'minlevel', 'entryprefix' ]
     , unknown: function(a) { return (a[0] == '-' ? (console.error('Unknown option(s): ' + a), printUsageAndExit(true)) : true); }
     });
 
@@ -119,7 +119,7 @@ var dryRun = argv.d || argv.dryrun || false;
 var maxHeaderLevel = argv.m || argv.maxlevel;
 if (maxHeaderLevel && isNaN(maxHeaderLevel)) { console.error('Max. heading level specified is not a number: ' + maxHeaderLevel), printUsageAndExit(true); }
 
-var minHeaderLevel = argv.n || argv.minlevel || 1;
+var minHeaderLevel = argv.minlevel || 1;
 if (minHeaderLevel && isNaN(minHeaderLevel) || minHeaderLevel < 0) { console.error('Min. heading level specified is not a positive number: ' + minHeaderLevel), printUsageAndExit(true); }
 else if (minHeaderLevel && minHeaderLevel > 2) { console.error('Min. heading level: ' + minHeaderLevel + ' is not currently supported as greater than 2'), printUsageAndExit(true); }
 
