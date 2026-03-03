9ec94fd62b8db7781fe6ae5d07a2745e12850bf6
James Thompson <thompson.tomo@outlook.com>
2026-03-03 12:07:42 +1100

    refactor to improve testability

--- test/transform.js
+++ test/transform.js
@@ -3,6 +3,7 @@
 
 var test = require('tap').test
   , transform = require('../lib/transform')
+  , contentGenerator = require('../lib/content-generation')
 
 function inspect(obj, depth) {
   console.log(require('util').inspect(obj, false, depth || 5, true));
@@ -16,10 +17,15 @@ function check(md, anchors, mode, maxHeaderLevel, minHeaderLevel, minTocItems, t
     var data = res.data.split('\n');
 
     // rig our expected value to include the wrapper
-    var startLines = transform.start.split('\n')
+    var legacy = contentGenerator.legacyPragma(syntax || 'md');
+    var startLines = legacy.start.split('\n')
       , anchorLines = anchors.split('\n')
-      , endLines = transform.end.split('\n')
-      , mdLines = md.split('\n')
+      , endLines = legacy.end.split('\n')
+      , mdLines = md.split('\n');
+    
+    if (legacy.header) {
+      startLines = startLines.concat(legacy.header.split('\n'));
+    }
 
     var rig = startLines
       .concat(anchorLines.slice(0, -2))
