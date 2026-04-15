6dbeaeeb321ff16e8ef519a927a8cbe9b81823c7
James Thompson <thompson.tomo@outlook.com>
2026-04-16 02:36:28 +1000

    unrevert

--- test/transform.js
+++ test/transform.js
@@ -50,6 +50,25 @@ check(
 )
 
 check(
+    [ '# My Module using \\r line endings'
+    , 'Some text here'
+    , '## API'
+    , '### Method One'
+    , 'works like this'
+    , '### Method Two'
+    , '#### Main Usage'
+    , 'some main usage here'
+    ].join('\r')
+  , [ '**Table of Contents**  *generated with [DocToc](https://github.com/thlorenz/doctoc)*\r\r'
+    , '- [My Module using \\r line endings](#my-module-using-r-line-endings)\r'
+    ,   '  - [API](#api)\r'
+    ,     '    - [Method One](#method-one)\r'
+    ,     '    - [Method Two](#method-two)\r'
+    ,         '      - [Main Usage](#main-usage)\r\r\r'
+    ].join('')
+)
+
+check(
     [ '# My Module using \\r\\n line endings'
     , 'Some text here'
     , '## API'
@@ -60,7 +79,25 @@ check(
     , 'some main usage here'
     ].join('\r\n')
   , [ '**Table of Contents**  *generated with [DocToc](https://github.com/thlorenz/doctoc)*\r\n\r\n'
-    , '- [My Module using \\r\\n line endings](#my-module-using-rn-line-endings)\r\n'
+    , '- [My Module using \\r\\n line endings](#my-module-using-rn-line-endings)\r\n'    ,   '  - [API](#api)\r\n'
+    ,     '    - [Method One](#method-one)\r\n'
+    ,     '    - [Method Two](#method-two)\r\n'
+    ,         '      - [Main Usage](#main-usage)\r\n\r\n\r\n'
+    ].join('')
+)
+
+check(
+    [ '# My Module using mixed line endings\r'
+    , 'Some text here'
+    , '## API'
+    , '### Method One'
+    , 'works like this\r'
+    , '### Method Two'
+    , '#### Main Usage\r'
+    , 'some main usage here'
+    ].join('\n')
+  , [ '**Table of Contents**  *generated with [DocToc](https://github.com/thlorenz/doctoc)*\n\n'
+    , '- [My Module using mixed line endings](#my-module-using-mixed-line-endings)\n'
     ,   '  - [API](#api)\n'
     ,     '    - [Method One](#method-one)\n'
     ,     '    - [Method Two](#method-two)\n'
