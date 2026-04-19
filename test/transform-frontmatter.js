e43be0a6e9dc720cec1b3a48551d6fd6ccefb029
James Thompson <thompson.tomo@outlook.com>
2026-04-19 13:27:14 +1000

    Add more tests

--- test/transform-frontmatter.js
+++ test/transform-frontmatter.js
@@ -12,7 +12,7 @@ test('\ngiven a file that includes Json frontmatter', function (t) {
       res.data
     , [ ';;;',
         'title: JSON Front Matter',
-        ';;;',
+        ';;; ',
         '',
         '<!-- START doctoc generated TOC please keep comment here to allow auto update -->',
         '<!-- DON\'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->',
@@ -40,7 +40,7 @@ test('\ngiven a file that includes Toml frontmatter', function (t) {
       res.data
     , [ '+++',
         'title: TOML Front Matter',
-        '+++',
+        '+++ ',
         '',
         '<!-- START doctoc generated TOC please keep comment here to allow auto update -->',
         '<!-- DON\'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->',
@@ -70,6 +70,8 @@ test('\ngiven a file that includes yaml frontmatter', function (t) {
         'title: Yaml Front Matter',
         '---',
         '',
+        '# Title',
+        '',
         '<!-- START doctoc generated TOC please keep comment here to allow auto update -->',
         '<!-- DON\'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->',
         '**Table of Contents**  *generated with [DocToc](https://github.com/thlorenz/doctoc)*',
@@ -78,7 +80,7 @@ test('\ngiven a file that includes yaml frontmatter', function (t) {
         '',
         '<!-- END doctoc generated TOC please keep comment here to allow auto update -->',
         '',
-        '# Heading',
+        '## Heading',
         '',
         'Your regular Markdown content follows...',
         '' ].join('\n')  
@@ -111,6 +113,77 @@ test('\ngiven a file that includes invalid frontmatter', function (t) {
         'Your regular Markdown content follows...',
         '' ].join('\n')  
     , 'generates correct toc for file with invalid frontmatter'
+  )
+
+  t.end()
+})
+
+test('\ngiven a file that has only yaml frontmatter and empty line', function (t) {
+  var content = require('fs').readFileSync(__dirname + '/fixtures/readme-frontmatter-blank.md', 'utf8');
+  var res = transform(content.trim() + '\n');
+
+  t.same(
+      res.data
+    , [ '---',
+        'title: Yaml Front Matter',
+        '---',
+        '',
+        '<!-- START doctoc generated TOC please keep comment here to allow auto update -->',
+        '<!-- DON\'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->',
+        '<!-- END doctoc generated TOC please keep comment here to allow auto update -->',
+        '' ].join('\n')  
+    , 'generates correct toc for file with only yaml frontmatter'
+  )
+
+  t.end()
+})
+
+test('\ngiven a file that has only yaml frontmatter and no trailing whitespace', function (t) {
+  var content = require('fs').readFileSync(__dirname + '/fixtures/readme-frontmatter-blank.md', 'utf8');
+  var res = transform(content.trim());
+
+  t.same(
+      res.data
+    , [ '---',
+        'title: Yaml Front Matter',
+        '---',
+        '',
+        '<!-- START doctoc generated TOC please keep comment here to allow auto update -->',
+        '<!-- DON\'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->',
+        '<!-- END doctoc generated TOC please keep comment here to allow auto update -->',
+        '' ].join('\n')  
+    , 'generates correct toc for file with only yaml frontmatter'
+  )
+
+  t.end()
+})
+
+test('\ngiven a file that includes yaml frontmatter and process all', function (t) {
+  var content = require('fs').readFileSync(__dirname + '/fixtures/readme-frontmatter-yaml.md', 'utf8');
+  var res = transform(contentmode, undefined, undefined, undefined, undefined, undefined, undefined, true);
+
+  t.same(
+      res.data
+    , [ '---',
+        'title: Yaml Front Matter',
+        '---',
+        '',
+        '# Title',
+        '',
+        '<!-- START doctoc generated TOC please keep comment here to allow auto update -->',
+        '<!-- DON\'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->',
+        '**Table of Contents**  *generated with [DocToc](https://github.com/thlorenz/doctoc)*',
+        '',
+        '- [Title](#title)',
+        '  - [Heading](#heading)',
+        '',
+        '<!-- END doctoc generated TOC please keep comment here to allow auto update -->',
+        '',
+        '## Heading',
+        '',
+        'Your regular Markdown content follows...',
+        '' ].join('\n')  
+    , 'generates correct toc for file with yaml frontmatter'
   )
 
   t.end()
